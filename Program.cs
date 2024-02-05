f03a70c8a04f1f15dbb91f3f9a2f02a988ca3102
Evgeniy <evgeniy.ryakhovskiy@gmail.com>
2024-02-05 17:38:46 +0300

    Решили 3 задачи

--- Program.cs
+++ Program.cs
@@ -1,1 +1,53 @@
-﻿a
+﻿// Задача 1:
+
+// int a = 322;
+// if (a % 7 == 0 && a % 23 == 0)
+// {
+//     Console.WriteLine("Да");
+// }
+
+
+// // Задача 2:
+
+// int x = -1;
+// int y = -2;
+// if (x > 0 && y > 0)
+// {
+//     Console.WriteLine ("I Четверть");
+// }
+// if (x < 0 && y > 0)
+// {
+//     Console.WriteLine("II четверть");
+// }
+// if (x < 0 && y < 0)
+// {
+//     Console.WriteLine("III четверть");
+// }
+// if (x > 0 && y < 0)
+// {
+//     Console.WriteLine("IV четверть");
+// }
+
+// Задача 3:
+
+// int number = 69;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// if (number > 9 && number < 100)
+//     if(firstDigit > secondDigit)
+//     {
+//         Console.WriteLine(firstDigit);
+//     }
+//     else
+//     {
+//         Console.WriteLine(secondDigit);
+//     }
+    
+// Задача 4:
+
+int N = 568;
+int negativeN = 1;
+if (N > 0)
+{
+    Console.WriteLine 
+}
